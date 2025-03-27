f8e12ca5c7ca69251ff5f092e47acd482e89b1e6
SuraBaka <eggytiowandana1@gmail.com>
2025-03-27 13:50:57 +0700

    harus bisa

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ buttonListAlat.forEach(name => {
     let button = document.createElement("a");
     button.textContent = name;
     button.className = "button";
-    button.href = `/LetYouKnowKarawitan/Screens/${name.toLowerCase().replace(/ /g, "-")}.html`;
+    button.href = `/Screens/${name.toLowerCase().replace(/ /g, "-")}.html`;
     buttonsContainerAlat.appendChild(button);
 });
 
@@ -28,7 +28,7 @@ buttonListNotasi.forEach(name => {
     let button = document.createElement("a");
     button.textContent = name;
     button.className = "button";
-    button.href = `/LetYouKnowKarawitan/Screens/${name.toLowerCase().replace(/ /g, "-")}.html`;
+    button.href = `/Screens/${name.toLowerCase().replace(/ /g, "-")}.html`;
     buttonsContainerNotasi.appendChild(button);
 });
 
